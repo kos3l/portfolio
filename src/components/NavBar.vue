b09ba583147b7da6d918d7270b43b8672f998348
kos3l <71730597+kos3l@users.noreply.github.com>
2021-04-17 18:15:14 +0200

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <v-toolbar flat app id="nav">
+      <v-toolbar-title>
+        
+        <button class="text-uppercase">
+          <span class="font-weight-bold">About</span>
+        </button>
+
+        <button class="text-uppercase">
+          <span class="font-weight-bold"> Contact</span>
+        </button>
+
+      </v-toolbar-title>
+      
+
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    data() {
+
+    },
+}
+</script>
+
+<style>
+#nav {
+  border: 2px solid black;
+  
+}
+</style>
