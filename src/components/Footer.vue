c77aff83366c210d953a8e5326991cf6f3e91812
kos3l <71730597+kos3l@users.noreply.github.com>
2021-04-29 12:01:26 +0200

    footer finished without animations

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,13 @@
 <template>
 
-    <v-row id="footer-container">
+    <v-row id="footer-container" class="d-none d-sm-flex">
         <v-col id="inner-footer">
             <a href="https://github.com/kos3l" target="_blank" id="some"> @ GITHUB </a>
             <a href="https://www.linkedin.com/feed/" target="_blank" id="some"> @ LINKEDIN </a>
             <a href="https://www.instagram.com/pimpoqe/" target="_blank" id="some"> @ INSTAGRAM </a>
+        </v-col>
+        <v-col id="second-inner-footer">
+            <p id="some-fun">I have a cat named Kropka, she's cute</p>
         </v-col>
     </v-row>
 </template>
@@ -23,26 +26,47 @@ export default {
 
 #footer-container{
     width: 100vw;
-    height: 10vh;
+    height: 12vh;
     background-color: black;
+    flex-direction: row;
+    bottom: 0;
+    position: absolute;
     
 
 }
 #inner-footer{
     display: flex;
-    justify-content: space-around;
+    justify-content: left;
     align-items: center;
     flex-direction: row;
 
 }
+#inner-footer a {
+    margin-left: 30px;
+}
 #some{
     text-decoration: none;
     color: white;
-    font-size:27px;
+    font-size:24px;
     font-weight: 900;
-
+}
+#second-inner-footer{
+    background-color: black;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-right: 30px;
+    height: 100%;
 
 }
+#some-fun{
+    color: white;
+    font-size:24px;
+    font-weight: 900;
+    margin-bottom: 0;
+}
+
+
 
 
 </style>
