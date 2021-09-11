f46f69e4ff699224a8382d3426f5c8fdeeb68a78
kos3l <71730597+kos3l@users.noreply.github.com>
2021-09-11 18:23:34 +0200

    cursour

--- src/components/layout/DarkModeToggle.vue
+++ src/components/layout/DarkModeToggle.vue
@@ -1,53 +1,52 @@
 <template>
-
-    <div class="toggle-wrapper">
-        <label class="toggle">
-            <input type="checkbox" 
-            :checked="(mode === 'night') ? 'checked' : false"
-            @change="$emit('toggle')"/>
-            <v-img  id="icon-dark"  :src="require('/src/assets/circle-dark.svg')"></v-img>
-        </label>
-    </div>
-
+  <div class="toggle-wrapper">
+    <label class="toggle">
+      <input
+        type="checkbox"
+        :checked="mode === 'night' ? 'checked' : false"
+        @change="$emit('toggle')"
+      />
+      <v-img
+        id="icon-dark"
+        :src="require('/src/assets/circle-dark.svg')"
+      ></v-img>
+    </label>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['mode']
-
-}
+  props: ["mode"],
+};
 </script>
 
 <style>
-.toggle-wrapper{
-    height: 100%;
-    width: 5vw;
-    display: flex;
-    justify-content: center;
+.toggle-wrapper {
+  height: 100%;
+  width: 5vw;
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
 }
-.toggle{
+.toggle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.toggle input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+@media screen and (max-width: 600px) {
+  .toggle {
     display: flex;
     justify-content: center;
     align-items: center;
-}
-.toggle input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-}
-    @media screen and (max-width:600px) {
-
-        .toggle{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 8vw;
-        }
-        .toggle-wrapper{
-            width: 8vw;
-    }
-
-    }
-
-
-</style>r
+    width: 8vw;
+  }
+  .toggle-wrapper {
+    width: 8vw;
+  }
+}</style
+>r
